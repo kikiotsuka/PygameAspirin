1a0810a8b5fc3476846b7fe0f5322a71ce5ffd23
kikiotsuka <mitsuru.otsuka@gmail.com>
2013-12-18 22:16:56 -0800

    added more code

--- aspirin.py
+++ aspirin.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import pygame, sys
 from pygame.locals import *
+from random import randint
 
 #constants
 screenwidth = 800
@@ -16,6 +17,7 @@ class Player:
 		self.collected = 0
 
 	def move(self, l, r, u, d):
+		global screenwidth, screenheight
 		if l:
 			x -= pspeed
 			if x < 0:
@@ -32,16 +34,27 @@ class Player:
 			y += pspeed
 			if y > screenheight:
 				y = screenheight - r
+		def getpos(self):
+			return (rectangle.left, rectangle.top)
 
+
+class PointBall:
+	def __init__(self, (x, y)):
+		self.r = 3
+		self.rectangle = Rect(x - r, y - r, r * 2, r * 2)
+
+	def getpos(self):
+		return (rectangle.left, rectangle.top)
 
 class Ball:
-	def __init__(self, x, y, direction, velocity):
+	def __init__(self, (x, y), direction, velocity):
 		self.r = 3
 		self.rectangle = Rect(x - r, y - r, r * 2, r * 2)
 		self.direction = direction
 		self.velocity = velocity
 
 	def move(self):
+		global screenwidth, screenheight
 		if direction == 'left':
 			rectangle.x -= velocity
 			if rectangle.left < 0:
@@ -63,8 +76,34 @@ class Ball:
 				rectangle.y = screenheight - r
 				direction = 'up'
 
+	def getpos(self):
+		return (rectangle.left, rectangle.top)
+
+
+def randloc():
+	global screenwidth, screenheight
+	return (randint(5, screenwidth), randint(5, screenheight))
+
+
 pygame.init()
 fpsClock = pygame.time.Clock()
 
 windowSurfaceObj = pygame.display.set_mode((screenwidth, screenheight))
 pygame.display.set_caption('Aspirin, game by Mitsuru Otsuka')
+
+fontObj = pygame.font.Font('freesansbold.ttf', 32)
+gameover = 'Game Over'
+yourscore = 'Your Score:'
+
+balllist = []
+pointball = PointBall(randloc())
+player = Player()
+isPlaying = True
+left=right=up=down=False
+while isPlaying:
+	windowSurfaceObj.fill(whiteColor)
+	for i, b in enumerate(balllist): #move balls then draw them
+		balllist[i].move()
+		pygame.draw.circle(windowSurfaceObj, pygame.Color(255, 0, 0), b.getpos(), b.r)
+	pygame.draw.circle(windowSurfaceObj, pygame.Color(0, 255, 0), pointball.getpos(), pointball.r)
+	pygame.draw.circle(windowSurfaceObj, pygame.Color(0, 0, 255), player.getpos(), player.r)
